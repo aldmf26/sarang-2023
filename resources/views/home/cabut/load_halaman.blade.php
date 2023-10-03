da40cdc0b667b3d18a7f26356a91ea92ab10ab4a
aldmf26 <aldimf26@gmail.com>
2023-10-03 10:35:39 +0800

    32

--- resources/views/home/cabut/load_halaman.blade.php
+++ resources/views/home/cabut/load_halaman.blade.php
@@ -4,22 +4,23 @@
         <table class="table table-hover table-bordered" style="border:1px solid #97a1c3" id="tableHalaman">
             <thead>
                 <tr>
-                    <th class="dhead ">#</th>
-                    <th class="dhead ">No Box</th>
-                    <th class="dhead ">Anak</th>
-                    <th class="dhead ">Tgl Terima Brg</th>
-                    <th class="text-end dhead ">Pcs Awal</th>
-                    <th class="text-end dhead ">Gr Awal</th>
-                    <th class="text-end dhead ">Gr Flx</th>
-                    <th class="text-end dhead ">Pcs Akhir</th>
-                    <th class="text-end dhead ">Gr Akhir</th>
-                    <th class="text-end dhead ">EOT</th>
-                    <th class="text-end dhead ">Pcs Hcr</th>
-                    <th class="text-end dhead ">Susut</th>
-                    <th class="text-end dhead ">Rp Trgt</th>
-                    <th class="text-end dhead ">Ttl Gaji</th>
-                    <th class="dhead ">Status</th>
-                    <th class="dhead " width="100">
+                    <th class="dhead">#</th>
+                    <th class="dhead" width="50">Bulan</th>
+                    <th class="dhead">No Box</th>
+                    <th class="dhead">Anak</th>
+                    <th class="dhead">Tgl Terima Brg</th>
+                    <th class="text-end dhead">Pcs Awal</th>
+                    <th class="text-end dhead">Gr Awal</th>
+                    <th class="text-end dhead">Gr Flx</th>
+                    <th class="text-end dhead">Pcs Akhir</th>
+                    <th class="text-end dhead">Gr Akhir</th>
+                    <th class="text-end dhead">EOT</th>
+                    <th class="text-end dhead">Pcs Hcr</th>
+                    <th class="text-end dhead">Susut</th>
+                    <th class="text-end dhead">Rp Trgt</th>
+                    <th class="text-end dhead">Ttl Gaji</th>
+                    <th class="dhead">Status</th>
+                    <th class="dhead" width="50">
                         <center>
                             <input style="text-align: center" type="checkbox" class="form-check" id="cekSemuaTutup">
                         </center>
@@ -30,6 +31,8 @@
                 @foreach ($cabut as $no => $d)
                     <tr>
                         <td>{{ $no + 1 }}</td>
+                        <td>{{ date('M y', strtotime($d->tgl_terima)) }}</td>
+
                         <td>
                             <a href="#" data-bs-toggle="modal" data-bs-target="#detail" class="detail"
                                 id_cabut="{{ $d->id_cabut }}">{{ $d->no_box }}</a>
