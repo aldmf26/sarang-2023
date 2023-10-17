5254fbb25d0e408eca3603730653af98a4678b7e
aldmf26 <aldimf26@gmail.com>
2023-10-17 09:53:29 +0800

    32

--- resources/views/home/cabut/load_modal_akhir.blade.php
+++ resources/views/home/cabut/load_modal_akhir.blade.php
@@ -107,9 +107,9 @@
                             </select>
                         </td>
                         <td>
-                            <button style="font-size: 12px"
+                            {{-- <button style="font-size: 12px"
                                 class="btn btn-sm btn-danger cancelCabutAkhir"
-                                type="button" count="{{ $i + 1 }}" id_cabut="{{ $d->id_cabut }}">Cancel</button>
+                                type="button" count="{{ $i + 1 }}" id_cabut="{{ $d->id_cabut }}">Cancel</button> --}}
                             <button style="font-size: 12px"
                                 class="btn btn-sm btn-{{ empty($d->gr_akhir) ? 'warning' : 'primary' }} saveCabutAkhir"
                                 type="button" count="{{ $i + 1 }}">Save</button>
