cb6f361b641f93416b9d9842dd25c3d3044d2c7a
aldmf26 <aldimf26@gmail.com>
2023-10-17 08:40:20 +0800

    32

--- app/Http/Controllers/CabutController.php
+++ app/Http/Controllers/CabutController.php
@@ -161,7 +161,8 @@ class CabutController extends Controller
             ->join('tb_anak as b', 'a.id_anak', 'b.id_anak')
             ->join('tb_kelas as c', 'a.id_kelas', 'c.id_kelas')
             ->where([['a.no_box', '!=', '9999'], ['a.penutup', 'T']])
-            ->orderBY('a.selesai', 'ASC');
+            ->orderBY('a.selesai', 'ASC')
+            ->orderBY('a.tgl_terima', 'ASC');
 
         if (auth()->user()->posisi_id != 1) {
             $cabut->where('a.id_pengawas', $id);
