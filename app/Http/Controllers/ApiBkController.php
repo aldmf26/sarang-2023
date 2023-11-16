8d34958f753ba5c0c279b6f1815df3acd5ea7c44
nadws <nandw567@gmail.com>
2023-11-16 14:38:17 +0800

    818

--- app/Http/Controllers/ApiBkController.php
+++ app/Http/Controllers/ApiBkController.php
@@ -10,13 +10,17 @@ class ApiBkController extends Controller
     public function sarang(Request $r)
     {
         $cabut = ApiBkModel::datacabut($r->no_lot);
+        $cetak = ApiBkModel::datacetak($r->no_lot);
+        $sortir = ApiBkModel::datasortir($r->no_lot);
+
         $response = [
             'status' => 'success',
             'message' => 'Data Sarang berhasil diambil',
             'data' => [
                 'cabut' => $cabut,
+                'cetak' => $cetak,
+                'sortir' => $sortir
             ],
-
         ];
         return response()->json($response);
     }
--- app/Models/ApiBkModel.php
+++ app/Models/ApiBkModel.php
@@ -11,10 +11,30 @@ class ApiBkModel extends Model
     use HasFactory;
     public static function datacabut($no_lot)
     {
-        $result = DB::select("SELECT a.no_lot, sum(b.pcs_awal) as pcs_awal, sum(b.gr_awal) as gr_awal, sum(b.rupiah) as rupiah 
+        $result = DB::selectOne("SELECT a.no_lot, sum(b.pcs_awal) as pcs_awal, sum(b.gr_awal) as gr_awal, sum(b.rupiah) as rupiah 
         FROM bk as a 
         left join cabut as b on b.no_box = a.no_box
-        where a.no_lot = ?;
+        where a.no_lot = ? and b.selesai = 'T';
+        ", [$no_lot]);
+
+        return $result;
+    }
+    public static function datacetak($no_lot)
+    {
+        $result = DB::selectOne("SELECT a.no_lot, sum(b.pcs_awal) as pcs_awal, sum(b.gr_awal) as gr_awal, sum(b.rp_pcs * b.pcs_awal ) as rupiah 
+        FROM bk as a 
+        left join cetak as b on b.no_box = a.no_box
+        where a.no_lot = ? and b.selesai = 'T';
+        ", [$no_lot]);
+
+        return $result;
+    }
+    public static function datasortir($no_lot)
+    {
+        $result = DB::selectOne("SELECT a.no_lot, sum(b.pcs_awal) as pcs_awal, sum(b.gr_awal) as gr_awal, sum(b.rp_target ) as rupiah 
+        FROM bk as a 
+        left join sortir as b on b.no_box = a.no_box
+        where a.no_lot = ? and b.selesai = 'T';
         ", [$no_lot]);
 
         return $result;
