98e8eda1c19c73eb7d8f24263dbee9f63c99a57d
aldmf26 <aldimf26@gmail.com>
2023-11-16 11:30:07 +0800

    321

--- resources/views/home/bk/index.blade.php
+++ resources/views/home/bk/index.blade.php
@@ -11,10 +11,8 @@
 
         <x-theme.button href="{{ route('bk.add', ['kategori' => $kategori]) }}" icon="fa-plus" addClass="float-end"
             teks="Tambah" />
-            @if (auth()->user()->name == 'sinta')
-            <x-theme.button modal="Y" idModal="import" href="#" icon="fa-upload" addClass="float-end"
-                teks="Import" />
-            @endif
+        <x-theme.button modal="Y" idModal="import" href="#" icon="fa-upload" addClass="float-end"
+            teks="Import" />
         <x-theme.btn_filter />
     </x-slot>
 
@@ -78,45 +76,45 @@
         </section>
         <form action="{{ route('bk.import') }}" enctype="multipart/form-data" method="post">
             @csrf
-        <x-theme.modal size="modal-lg" idModal="import" title="Import Bk">
-            <div class="row">
-                <table>
-                    <tr>
-                        <td width="100" class="pl-2">
-                            <img width="80px" src="{{ asset('/img/1.png') }}" alt="">
-                        </td>
-                        <td>
-                            <span style="font-size: 20px;"><b> Download Excel template</b></span><br>
-                            File ini memiliki kolom header dan isi yang sesuai dengan data menu
-                        </td>
-                        <td>
-                            <a href="{{ route('bk.template') }}" class="btn btn-primary btn-sm"><i
-                                    class="fa fa-download"></i> DOWNLOAD
-                                TEMPLATE</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="3">
-                            <hr>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td width="100" class="pl-2">
-                            <img width="80px" src="{{ asset('/img/2.png') }}" alt="">
-                        </td>
-                        <td>
-                            <span style="font-size: 20px;"><b> Upload Excel template</b></span><br>
-                            Setelah mengubah, silahkan upload file.
-                        </td>
-                        <td>
-                            <input type="file" name="file" class="form-control">
-                        </td>
-                    </tr>
-                </table>
+            <x-theme.modal size="modal-lg" idModal="import" title="Import Bk">
+                <div class="row">
+                    <table>
+                        <tr>
+                            <td width="100" class="pl-2">
+                                <img width="80px" src="{{ asset('/img/1.png') }}" alt="">
+                            </td>
+                            <td>
+                                <span style="font-size: 20px;"><b> Download Excel template</b></span><br>
+                                File ini memiliki kolom header dan isi yang sesuai dengan data menu
+                            </td>
+                            <td>
+                                <a href="{{ route('bk.template') }}" class="btn btn-primary btn-sm"><i
+                                        class="fa fa-download"></i> DOWNLOAD
+                                    TEMPLATE</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">
+                                <hr>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td width="100" class="pl-2">
+                                <img width="80px" src="{{ asset('/img/2.png') }}" alt="">
+                            </td>
+                            <td>
+                                <span style="font-size: 20px;"><b> Upload Excel template</b></span><br>
+                                Setelah mengubah, silahkan upload file.
+                            </td>
+                            <td>
+                                <input type="file" name="file" class="form-control">
+                            </td>
+                        </tr>
+                    </table>
 
-            </div>
-        </x-theme.modal>
-     
+                </div>
+            </x-theme.modal>
+
 
     </x-slot>
     @section('scripts')
