5f0ffb64f341415160be3d1ade07ebc5d7a18188
aldmf26 <aldimf26@gmail.com>
2024-03-12 08:39:36 +0800

    update

--- app/Http/Controllers/OpnameController.php
+++ app/Http/Controllers/OpnameController.php
@@ -407,7 +407,7 @@ class OpnameController extends Controller
                 'body' => [
                     'pcs' => $bkCbtPgwsSelesai->pcs_akhir,
                     'gr' => $bkCbtPgwsSelesai->gr_akhir + $bkCbtPgwsSelesai->gr_eoeo_akhir,
-                    'ttl_rp' => $bkCbtPgwsSelesai->ttl_rp + $bkCbtPgwsSelesai->eo_ttl_rp,
+                    'ttl_rp' => $bkCbtAwal->ttl_rp_selesai,
                 ],
             ],
             [
