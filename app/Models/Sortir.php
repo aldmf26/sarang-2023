2c3e672416793211cb7b9dd74bbea707dc56e583
nadws <nandw567@gmail.com>
2024-07-12 11:21:33 +0800

    update Sortir.php

--- app/Models/Sortir.php
+++ app/Models/Sortir.php
@@ -92,7 +92,7 @@ class Sortir extends Model
         left join eo as e on e.no_box = a.no_box
         left join users as f on f.id = a.id_penerima
         left join eo as g on g.no_box = a.no_box
-        WHERE a.no_box not in(SELECT b.no_box FROM sortir as b where b.id_anak != 0) and a.kategori = 'sortir' and a.id_anak = 0;");
+        WHERE a.no_box not in(SELECT b.no_box FROM sortir as b where b.id_anak != 0) and a.kategori = 'sortir' $id_pengawas;");
         return $result;
     }
     public static function sortir_proses($id_user)
