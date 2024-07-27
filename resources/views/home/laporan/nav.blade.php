7b5e4c69e38e4376d09fa65d59305133a8aad396
aldmf26 <aldimf26@gmail.com>
2024-07-27 08:21:13 +0800

    update

--- resources/views/home/laporan/nav.blade.php
+++ resources/views/home/laporan/nav.blade.php
@@ -4,10 +4,10 @@
 <ul class="nav nav-pills float-start">
     <li class="nav-item">
         <a class="nav-link {{ $rot == 'laporanakhir.index' ? 'active' : '' }}" aria-current="page"
-            href="{{ route('laporanakhir.index') }}">Laporan Perbox</a>
+            href="{{ route('laporanakhir.index') }}">Laporan Perpartai</a>
     </li>
-    <li class="nav-item">
+    {{-- <li class="nav-item">
         <a class="nav-link {{ $rot == 'laporanakhir.lapPartai' ? 'active' : '' }}" aria-current="page"
             href="{{ route('laporanakhir.lapPartai') }}">Laporan partai</a>
-    </li>
+    </li> --}}
 </ul>
