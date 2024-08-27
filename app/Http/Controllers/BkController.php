9868afe5ca26a67d8dcd32151f463f0ddb898948
aldmf26 <aldimf26@gmail.com>
2024-08-27 09:01:29 +0800

    update

--- app/Http/Controllers/BkController.php
+++ app/Http/Controllers/BkController.php
@@ -276,7 +276,7 @@ class BkController extends Controller
                     'ket' => $row[3],
                     'warna' => $row[4],
                     'tgl' => date('Y-m-d'),
-                    'pengawas' => $row[6] == 'cabut' ? 'sinta' : 'siti fatimah',
+                    'pengawas' => 'siti fatimah',
                     'penerima' => $row['7'],
                     'pcs_awal' => $row[5],
                     'gr_awal' => $row[6],
