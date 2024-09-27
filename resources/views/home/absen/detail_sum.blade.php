a65f5bc43a97b0221e43a012d6faeea64a8df0d8
aldmf26 <aldimf26@gmail.com>
2024-09-27 08:17:17 +0800

    update

--- resources/views/home/absen/detail_sum.blade.php
+++ resources/views/home/absen/detail_sum.blade.php
@@ -72,7 +72,7 @@
                                         $tahunH = $date->format('Y');
 
                                         $getTgl = DB::table('absen')
-                                            ->where([['id_anak', $d->id_anak], ['bulan_dibayar', "$bulanDibayar"]])
+                                            ->where([['id_anak', $d->id_anak], ['bulan_dibayar', "9"]])
                                             ->count();
                                         $ttl += $getTgl ?? 0;
                                     @endphp
