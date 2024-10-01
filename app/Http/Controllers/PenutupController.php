8dad7b7ec1827a9231f48c9469f2c978a5150c37
aldmf26 <aldimf26@gmail.com>
2024-10-01 14:40:01 +0800

    updsa

--- app/Http/Controllers/PenutupController.php
+++ app/Http/Controllers/PenutupController.php
@@ -15,7 +15,7 @@ class PenutupController extends Controller
 
     public function getData($param)
     {
-        $bulan = date('m');
+        $bulan = 9;
         $tahun = date('Y');
         $pengawas = DB::select("SELECT b.id as id_pengawas,b.name FROM bk as a
                 JOIN users as b on a.penerima = b.id
