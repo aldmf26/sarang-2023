64b5dcfeb49a0682d14f578f2736d2188ee40f4f
aldmf26 <aldimf26@gmail.com>
2024-10-07 08:19:28 +0800

    updsa

--- app/Models/Grading.php
+++ app/Models/Grading.php
@@ -99,7 +99,8 @@ class Grading extends Model
         g.cost_str,
         g.cost_cu,
         akhir_sortir.ttl_rp as ttl_rp_sortir,
-        akhir_sortir.cost_kerja as cost_kerja_sortir
+        akhir_sortir.cost_kerja as cost_kerja_sortir,
+        akhir_sortir.no_box as no_box
         FROM grading as a 
         JOIN formulir_sarang as b on b.no_box = a.no_box_sortir AND b.kategori = 'grade'
         JOIN bk as e on e.no_box = b.no_box AND e.kategori = 'cabut'
