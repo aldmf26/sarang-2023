998c98b06c58eb1e0c3f548548034fc8e3b1f2f5
aldmf26 <aldimf26@gmail.com>
2024-10-10 15:31:23 +0800

    updsa

--- resources/views/home/cocokan/grading.blade.php
+++ resources/views/home/cocokan/grading.blade.php
@@ -99,7 +99,7 @@
                         </td>
                         <td class="text-end fw-bold text-danger ">
                             <a href="{{ route('opnamenew.grading') }}" target="_blank" class="text-danger">
-                                {{ number_format($sortir_akhir->gr + $opname->gr - $grading->gr - $grading_sisa->gr, 0) }}
+                               0
                             </a>
                         </td>
                         <td class="text-end text-danger fw-bold">
