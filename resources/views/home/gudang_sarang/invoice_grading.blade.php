fb726ec69566162568dd80c1d86b21eeff3b6c1e
aldmf26 <aldimf26@gmail.com>
2024-10-18 10:46:37 +0800

    dasd

--- resources/views/home/gudang_sarang/invoice_grading.blade.php
+++ resources/views/home/gudang_sarang/invoice_grading.blade.php
@@ -53,7 +53,6 @@
                                     $getSudahGrading = DB::table('grading')->where('no_invoice', $no_invoice)->first();
                                 @endphp
                                 @pgwsGrading
-                                    
                                     @if ($getSudahGrading)
                                     <a href="{{ route('gradingbj.detail_pengiriman', ['no_invoice' => $d->no_invoice]) }}">
                                         <span class="badge bg-primary">Detail</span>
@@ -77,7 +76,6 @@
                                         @csrf
                                         <input type="hidden" name="no_box" value="{{ $hasil }}">
                                         <button type="submit" name="submit" class="border-0 badge bg-success" value="grading">Grading</button>
-                                        
                                     </form> --}}
                                 @endpgwsGrading
 
