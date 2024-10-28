4126f63bebceb7964f57478693fdfc12fd2e1a36
aldmf26 <aldimf26@gmail.com>
2024-10-28 14:46:03 +0800

    update

--- resources/views/home/gradingbj/print_grading.blade.php
+++ resources/views/home/gradingbj/print_grading.blade.php
@@ -38,48 +38,51 @@
                 <i class="fa-solid fa-print"></i> Print
             </a>
         </div>
-        <div class="d-flex flex-wrap">
-        @foreach ($grading as $d)
-            <div style="width:25%; padding:10px;">
-                <table border="1">
-                    <tr>
-                        <td>No Nota</td>
-                        <td>:</td>
-                        <td>{{ $d->no_invoice }}</td>
-                    </tr>
-                    <tr>
-                        <td>Nama Partai</td>
-                        <td>:</td>
-                        <th>{{ $d->nm_partai }}</th>
-                    </tr>
-                    <tr>
-                        <td>Box Grading</td>
-                        <td>:</td>
-                        <th>{{ $d->box_pengiriman }}</th>
-                    </tr>
-                    <tr>
-                        <td>Grade</td>
-                        <td>:</td>
-                        <th><h5>{{ $d->grade }}</h5></th>
-                    </tr>
-                    <tr>
-                        <td>Pcs</td>
-                        <td>:</td>
-                        <td>{{ $d->pcs }}</td>
-                    </tr>
-                    <tr>
-                        <td>Gr</td>
-                        <td>:</td>
-                        <td>{{ $d->gr }}</td>
-                    </tr>
-                </table>
-            </div>
-        @endforeach
+        <div style="font-size: 15px; display: flex; flex-wrap: wrap; gap: 0; margin: 0; padding: 0;">
+            @foreach ($grading as $d)
+                <div style="width: 25%; box-sizing: border-box; padding: 0; margin: 0;">
+                    <table border="1" style="border-collapse: collapse; width: 100%; margin: 0; padding: 0;">
+                        <tr>
+                            <td style="padding: 5px;">No Nota</td>
+                            <td style="padding: 5px;">:</td>
+                            <td style="padding: 5px;">{{ $d->no_invoice }}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Nama Partai</td>
+                            <td style="padding: 5px;">:</td>
+                            <th style="padding: 5px;">{{ $d->nm_partai }}</th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Box Grading</td>
+                            <td style="padding: 5px;">:</td>
+                            <th style="padding: 5px;">{{ $d->box_pengiriman }}</th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Grade</td>
+                            <td style="padding: 5px;">:</td>
+                            <th style="padding: 5px;">
+                                <h5 style="margin: 0;">{{ $d->grade }}</h5>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Pcs</td>
+                            <td style="padding: 5px;">:</td>
+                            <td style="padding: 5px;">{{ $d->pcs }}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding: 5px;">Gr</td>
+                            <td style="padding: 5px;">:</td>
+                            <td style="padding: 5px;">{{ $d->gr }}</td>
+                        </tr>
+                    </table>
+                </div>
+            @endforeach
         </div>
+        
     </div>
-<script>
-    window.print()
-</script>
+    <script>
+        window.print()
+    </script>
 </body>
 
 </html>
