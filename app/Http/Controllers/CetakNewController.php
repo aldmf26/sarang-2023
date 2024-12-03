ce2295f1b905e61ccb7bb62e2025bf840f0dfedd
aldmf26 <aldimf26@gmail.com>
2024-12-03 13:19:22 +0800

    dsa

--- app/Http/Controllers/CetakNewController.php
+++ app/Http/Controllers/CetakNewController.php
@@ -1103,8 +1103,6 @@ class CetakNewController extends Controller
                 $pcs = $ambil->pcs_akhir;
                 $gr = $ambil->gr_akhir;
     
-    
-    
                 $data[] = [
                     'no_invoice' => $inv,
                     'no_box' => $ambil->no_box,
