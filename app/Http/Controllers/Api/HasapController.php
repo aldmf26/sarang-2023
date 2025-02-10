c25347f895adfc7e5f1f49c9a6c47e5fff358ad4
nadws <nandw567@gmail.com>
2025-02-10 15:14:29 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -10,7 +10,15 @@ class HasapController extends Controller
 {
     public function index()
     {
-        $data = DB::select("SELECT * FROM formulir_sarang as a where a.kategori = 'cabut' and a.selesai ='Y' ");
+        $data = DB::select("SELECT c.no_invoice, c.tanggal, b.name, sum(c.pcs_awal) as pcs, sum(c.gr_awal) as gr_awal
+        FROM cabut as a 
+        left join users as b on b.id = a.id_pengawas
+        join (
+            SELECT c.no_invoice, c.no_box, c.pcs_awal, c.gr_awal, c.tanggal
+            FROM formulir_sarang as c 
+            where c.kategori ='cabut'
+        ) as c on c.no_box = a.no_box
+        group by c.no_invoice;");
         return response()->json([
             'status' => 'success',
             'message' => 'success',
