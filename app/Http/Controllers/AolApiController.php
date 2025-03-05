973df26664fe222267d6034aa37aec207c7483b1
aldmf26 <aldimf26@gmail.com>
2025-03-05 11:14:09 +0800

    dsa

--- app/Http/Controllers/AolApiController.php
+++ app/Http/Controllers/AolApiController.php
@@ -32,7 +32,6 @@ class AolApiController extends Controller
         ];
 
         return $data;
-
     }
     public function index()
     {
@@ -40,26 +39,42 @@ class AolApiController extends Controller
             'title' => 'AOL API',
             'redirect_uri' => 'https://sarang.ptagafood.com/aol'
         ];
-        return view('aol.index',$data);
+        return view('aol.index', $data);
     }
 
     public function read()
     {
+
+        $url = "https://public.accurate.id/accurate/api/item/list.do";
+        $id_database = 1656763;
+        $session = "e48bc44b-db67-4d05-91d8-e29f32458404";
+
         $headers = [
             'Authorization' => 'bearer c284c7d1-4f3c-463a-a49a-575d645c3e61',
+            'X-Session-ID' => $session
         ];
-        $url = "https://account.accurate.id/api/db-list.do";
-        $response = Http::withHeaders($headers)
-                ->get($url);
 
-            // Cek jika request berhasil (status code 200)
-            if ($response->successful()) {
-                $result = $response->json();
-                return response()->json([
-                    'status' => 'success',
-                    'data' => $result,
-                ]);
-            }
+        $response = Http::withHeaders($headers)
+            ->asForm()
+            ->post($url, [
+                'fields' => 'id,name,no',
+                'filter.itemType' => 'INVENTORY',
+            ]);
+
+        // Cek jika request berhasil (status code 200)
+        if ($response->successful()) {
+            $result = $response->json();
+            return response()->json([
+                'status' => 'success',
+                'data' => $result,
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Gagal mendapatkan token',
+            'error' => $response->json(),
+        ], $response->status());
     }
 
     public function getToken()
@@ -100,7 +115,6 @@ class AolApiController extends Controller
                 'message' => 'Gagal mendapatkan token',
                 'error' => $response->json(),
             ], $response->status());
-
         } catch (\Exception $e) {
             // Tangani error jika ada masalah jaringan atau lainnya
             return response()->json([
