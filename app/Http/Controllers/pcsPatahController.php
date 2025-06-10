b309534a497d38ffd7738ab09377d3f7065bdb22
nadws <nandw567@gmail.com>
2025-06-10 11:01:50 +0800

    update pcsPatahController.php

--- app/Http/Controllers/pcsPatahController.php
+++ app/Http/Controllers/pcsPatahController.php
@@ -18,7 +18,7 @@ class pcsPatahController extends Controller
             ->leftJoin('eo as d', 'd.no_box', '=', 'a.no_box')
             ->where('a.kategori', 'cetak')
             ->where('a.no_invoice', $r->no_invoice)
-            ->selectRaw('a.id_formulir,a.sst_aktual,b.nm_partai, a.no_box, a.pcs_awal, a.gr_awal, b.tipe, b.ket, c.pcs_awal as pcs_cbt, c.gr_awal as gr_cbt, d.gr_eo_awal as gr_eo')
+            ->selectRaw('a.id_formulir,a.sst_aktual,b.nm_partai, a.no_box, a.pcs_awal, a.gr_awal, b.tipe, b.ket, c.pcs_awal as pcs_cbt, c.gr_awal as gr_cbt, d.gr_eo_awal as gr_eo, c.ket_hcr')
             ->get();
         dd($formulir);
 
