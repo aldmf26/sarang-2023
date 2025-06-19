21ffcd7419fab593616649deaa133709e8a5869e
nadws <nandw567@gmail.com>
2025-06-19 15:00:54 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -114,7 +114,7 @@ SELECT d.tgl_ambil as tgl, d.no_box, f.nm_partai, g.nama, 0 as pcs, sum(d.gr_eo_
         group by e.no_box
         ) as c on c.no_box = a.no_box
         where c.baru = 'baru' and  a.selesai = 'Y'
-        group by a.id_pengawas, a.tgl_terima
+        group by a.id_pengawas, a.tgl_ambil
         
         ;");
 
