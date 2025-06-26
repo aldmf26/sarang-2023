b1a7f06e480ffed6d90938e063242bf2f9e7c1a3
nadws <nandw567@gmail.com>
2025-06-26 15:52:33 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -286,7 +286,7 @@ SELECT d.tgl_ambil as tgl, d.no_box, f.nm_partai, g.nama, 0 as pcs, sum(d.gr_eo_
         GROUP_CONCAT(DISTINCT CONCAT(\"'\", a.nm_partai, \"'\") SEPARATOR ', ') AS nm_partai 
         FROM grading_partai as a
         JOIN pengiriman as b ON b.no_box = a.box_pengiriman
-        GROUP BY b.tgl");
+        GROUP BY b.tgl_input");
         return response()->json([
             'status' => 'success',
             'message' => 'success',
