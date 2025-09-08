9c96ae0e91a315ce8ce0cd386a5210e30f8ba6fb
nadws <nandw567@gmail.com>
2025-09-08 08:57:42 +0800

    update getcost_partai.blade.php

--- resources/views/home/opnamesusut/getcost_partai.blade.php
+++ resources/views/home/opnamesusut/getcost_partai.blade.php
@@ -137,7 +137,7 @@
                     <td></td>
                     <td class="text-end">{{ $formatNumber($cabut->pcs ?? 0) }}</td>
                     <td class="text-end">{{ $formatNumber($grAwalCabut) }}</td>
-                    <td class="text-end">{{ $formatNumber($cabut->pcs) }}</td>
+                    <td class="text-end">{{ $formatNumber($cabut->pcs ?? 0) }}</td>
                     <td class="text-end">{{ $formatNumber($grAkhirCabut) }}</td>
                     <td class="text-end">{{ $formatNumber($calculateSusut($grAkhirCabut, $grAwalCabut)) }}%</td>
                     <td class="text-end">{{ $formatNumber($cabutModal) }}</td>
