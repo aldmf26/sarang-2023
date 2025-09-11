9bc86ce70d53e4f45a54d5ab8ef2e7d5f9204b51
nadws <nandw567@gmail.com>
2025-09-11 14:19:14 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -405,33 +405,28 @@ SELECT d.tgl_ambil as tgl, d.tgl_serah as tgl_selesai, d.no_box, f.nm_partai, g.
         }
 
         $data = DB::select("SELECT 
-    b.no_barcode,
-    gp.grade,
-    SUM(gp.pcs) AS total_pcs,
-    SUM(gp.gr) AS total_gr,
-    GROUP_CONCAT(DISTINCT CONCAT('''', gp.nm_partai, '''') ORDER BY gp.nm_partai SEPARATOR ', ') AS nm_partai,
-    GROUP_CONCAT(
-        CONCAT(gp.nm_partai, ' : ', gp.pcs, ' pcs / ', gp.gr, ' gr')
-        ORDER BY gp.nm_partai SEPARATOR '<br>'
-    ) AS detail_partai
-FROM (
-    SELECT 
-        box_pengiriman,
-        grade,
-        -- perbaikan: hapus spasi ganda & trim
-        REGEXP_REPLACE(TRIM(nm_partai), '\\s+', ' ') AS nm_partai,
-        SUM(pcs) AS pcs,
-        SUM(gr) AS gr
-    FROM grading_partai
-    GROUP BY box_pengiriman, grade, 
-        REGEXP_REPLACE(TRIM(nm_partai), '\\s+', ' ')
-) AS gp
-JOIN pengiriman AS b ON b.no_box = gp.box_pengiriman
-WHERE b.tgl_input = '$tgl'
-GROUP BY b.no_barcode, gp.grade;
-
-
-
+        b.no_barcode,
+        gp.grade,
+        SUM(gp.pcs) AS total_pcs,
+        SUM(gp.gr) AS total_gr,
+        GROUP_CONCAT(CONCAT(\"'\", gp.nm_partai, \"'\") SEPARATOR ', ') AS nm_partai,
+        GROUP_CONCAT(
+            CONCAT(gp.nm_partai, ' : 'gp.pcs, ' pcs / ', gp.gr, ' gr')
+            ORDER BY gp.nm_partai SEPARATOR '<br>'
+        ) AS detail_partai
+    FROM (
+        SELECT 
+            box_pengiriman,
+            grade,
+            nm_partai,
+            SUM(pcs) AS pcs,
+            SUM(gr) AS gr
+        FROM grading_partai
+        GROUP BY box_pengiriman, grade, nm_partai
+    ) AS gp
+    JOIN pengiriman AS b ON b.no_box = gp.box_pengiriman
+    WHERE b.tgl_input = '$tgl'
+    GROUP BY b.no_barcode, gp.grade
 
         ");
         return response()->json([
