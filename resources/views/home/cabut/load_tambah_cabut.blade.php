45f7245d07181750da227140e2c49fd8e9168393
aldmf26 <aldimf26@gmail.com>
2025-11-10 14:38:11 +0800

    fix : cabut box loop

--- resources/views/home/cabut/load_tambah_cabut.blade.php
+++ resources/views/home/cabut/load_tambah_cabut.blade.php
@@ -30,75 +30,83 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($boxBk as $i => $x)
-                    <tr class="baris{{ $i + 1 }}">
-                        <td>
-                            <input type="text" class="form-control" readonly value="{{ auth()->user()->name }}">
-                            <input type="hidden" class="form-control" name="id_pengawas[]" readonly
-                                value="{{ auth()->user()->id }}">
-                        </td>
-                        <td>
-                            <input type="date" value="{{ date('Y-m-d') }}" class="form-control" name="tgl_terima[]">
-                            <input type="hidden" value="{{ $x->id_cabut }}" class="form-control" name="id_cabut[]">
-                        </td>
-                        <td>
-                            <select name="no_box[]" id="" class="select3 pilihBox" count="{{ $i + 1 }}">
-                                <option value="">Pilih Box</option>
-                                @foreach ($boxBk as $d)
-                                    @if ($d->gr_awal - $d->gr_cabut > 1)
-                                        <option value="{{ $d->no_box }}">{{ ucwords($d->no_box) }}</option>
-                                    @endif
-                                @endforeach
-                            </select>
-                        </td>
-                        <td>
-                            <span class="h6">{{ strtoupper($x->nama) }} <span
-                                    class="float-end">({{ $x->kelas }})</span></span>
-                            <input type="hidden" name="id_anak[]" value="{{ $x->id_anak }}">
+                @foreach ($getAnak as $i => $x)
+                    @foreach ($$boxBk as $box)
+                        <tr class="baris{{ $i + 1 }}">
+                            <td>
+                                <input type="text" class="form-control" readonly value="{{ auth()->user()->name }}">
+                                <input type="hidden" class="form-control" name="id_pengawas[]" readonly
+                                    value="{{ auth()->user()->id }}">
+                            </td>
+                            <td>
+                                <input type="date" value="{{ date('Y-m-d') }}" class="form-control"
+                                    name="tgl_terima[]">
+                                <input type="hidden" value="{{ $x->id_cabut }}" class="form-control"
+                                    name="id_cabut[]">
+                            </td>
+                            <td>
+                                <select name="no_box[]" id="" class="select3 pilihBox"
+                                    count="{{ $i + 1 }}">
+                                    <option value="">Pilih Box</option>
+                                    @foreach ($boxBk as $d)
+                                        @if ($d->gr_awal - $d->gr_cabut > 1)
+                                            <option @selected($d->no_box == $box->no_box) value="{{ $d->no_box }}">
+                                                {{ ucwords($d->no_box) }}</option>
+                                        @endif
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <span class="h6">{{ strtoupper($x->nama) }} <span
+                                        class="float-end">({{ $x->kelas }})</span></span>
+                                <input type="hidden" name="id_anak[]" value="{{ $x->id_anak }}">
 
-                            <input type="hidden" class="setHargaSatuanGr{{ $i + 1 }}">
-                            <input type="hidden" class="setHargaSatuanPcs{{ $i + 1 }}">
-                        </td>
-                        <td>
-                            <select name="hitung[]" id=""
-                                class="form-control pilihHitung pilihHitung{{ $i + 1 }}"
-                                count="{{ $i + 1 }}">
-                                <option value="">Pilih</option>
-                                <option value="1">Pcs</option>
-                                <option value="2">Gr</option>
-                                {{-- <option value="3">Spesial</option>
+                                <input type="hidden" class="setHargaSatuanGr{{ $i + 1 }}">
+                                <input type="hidden" class="setHargaSatuanPcs{{ $i + 1 }}">
+                            </td>
+                            <td>
+                                <select name="hitung[]" id=""
+                                    class="form-control pilihHitung pilihHitung{{ $i + 1 }}"
+                                    count="{{ $i + 1 }}">
+                                    <option value="">Pilih</option>
+                                    <option value="1">Pcs</option>
+                                    <option value="2">Gr</option>
+                                    {{-- <option value="3">Spesial</option>
                                 <option value="4">Eo</option> --}}
-                            </select>
-                        </td>
-                        <td>
+                                </select>
+                            </td>
+                            <td>
 
-                            <select name="id_paket[]" id=""
-                                class="select3 pilihAnak pilihAnak{{ $i + 1 }}" count="{{ $i + 1 }}">
-                                <option value="">Pilih</option>
-                            </select>
-                        </td>
-                        <td>
-                            <input type="text" class="form-control text-end setPcs setPcs{{ $i + 1 }}"
-                                value="0" id="pcsInput" name="pcs_awal[]" count="{{ $i + 1 }}">
-                        </td>
-                        <td>
-                            <input type="text" class="form-control text-end setGr setGr{{ $i + 1 }}"
-                                count="{{ $i + 1 }}" value="0" id="grInput" name="gr_awal[]">
-                        </td>
-                        <td>
-                            <input readonly type="text"
-                                class="form-control rupiahInput text-end setRupiah{{ $i + 1 }}" value="0">
-                            <input readonly type="hidden"
-                                class="form-control rupiahInput text-end rupiahBiasa{{ $i + 1 }}" value="0"
-                                name="rupiah[]">
-                        </td>
-                        <td align="center">
-                            <button type="button" class="btn rounded-pill hapusCabutRow"
-                                id_cabut="{{ $x->id_cabut }}" id_anak="{{ $x->id_anak }}"
-                                count="{{ $i + 1 }}"><i class="fas fa-trash text-danger"></i>
-                            </button>
-                        </td>
-                    </tr>
+                                <select name="id_paket[]" id=""
+                                    class="select3 pilihAnak pilihAnak{{ $i + 1 }}"
+                                    count="{{ $i + 1 }}">
+                                    <option value="">Pilih</option>
+                                </select>
+                            </td>
+                            <td>
+                                <input type="text" class="form-control text-end setPcs setPcs{{ $i + 1 }}"
+                                    value="0" id="pcsInput" name="pcs_awal[]" count="{{ $i + 1 }}">
+                            </td>
+                            <td>
+                                <input type="text" class="form-control text-end setGr setGr{{ $i + 1 }}"
+                                    count="{{ $i + 1 }}" value="0" id="grInput" name="gr_awal[]">
+                            </td>
+                            <td>
+                                <input readonly type="text"
+                                    class="form-control rupiahInput text-end setRupiah{{ $i + 1 }}"
+                                    value="0">
+                                <input readonly type="hidden"
+                                    class="form-control rupiahInput text-end rupiahBiasa{{ $i + 1 }}"
+                                    value="0" name="rupiah[]">
+                            </td>
+                            <td align="center">
+                                <button type="button" class="btn rounded-pill hapusCabutRow"
+                                    id_cabut="{{ $x->id_cabut }}" id_anak="{{ $x->id_anak }}"
+                                    count="{{ $i + 1 }}"><i class="fas fa-trash text-danger"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
                 @endforeach
             </tbody>
             <tbody id="tbh_baris">
