ba4cfeaaae040fed51eca5b45b550b2cd228fe06
nadws <nandw567@gmail.com>
2025-11-13 16:05:11 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -983,7 +983,7 @@ ON all_data.grade = done_data.grade;");
         left join bk as d on d.no_box = c.no_box and d.kategori = 'cabut'
         left join tb_anak as e on e.id_anak = c.id_anak
         left join users as f on f.id = c.id_pengawas
-        where c.no_box != '9999'  and c.id_kelas not in('126','166','152') and c.id_pengawas not in ('104','421','99','101','285')
+        where c.no_box != '9999'  and c.id_kelas not in('126','166','152' , '142') and c.id_pengawas not in ('104','421','99','101','285')
     
     	UNION ALL
     	
@@ -1007,7 +1007,7 @@ ON all_data.grade = done_data.grade;");
         left join bk as d on d.no_box = c.no_box and d.kategori = 'cabut'
         left join tb_anak as e on e.id_anak = c.id_anak
         left join users as f on f.id = c.id_pengawas
-        where c.no_box != '9999' and c.id_kelas not in('126','166','152') and c.id_pengawas not in ('104','421','99','101','285')
+        where c.no_box != '9999' and c.id_kelas not in('126','166','152','142') and c.id_pengawas not in ('104','421','99','101','285')
     
     
         ) AS hasil
@@ -1053,7 +1053,7 @@ ON all_data.grade = done_data.grade;");
   left join tb_anak as e on e.id_anak = c.id_anak
   left join hasil_wawancara as f on f.id_anak = e.id_anak
   left join tb_hancuran as g on g.no_box = c.no_box and g.kategori = 'cetak'
-  where c.no_box != '9999'
+  where c.no_box != '9999' and c.id_kelas not in('126','166','152','142') 
     
     UNION ALL 
     
@@ -1079,7 +1079,7 @@ ON all_data.grade = done_data.grade;");
   left join tb_anak as e on e.id_anak = c.id_anak
   left join hasil_wawancara as f on f.id_anak = e.id_anak
   left join tb_hancuran as g on g.no_box = c.no_box and g.kategori = 'cetak'
-  where c.no_box != '9999'
+  where c.no_box != '9999' and c.id_kelas not in('126','166','152','142') 
 
 ) AS hasil
 WHERE tgl_terima BETWEEN '2025-07-28' and now() and id_pengawas = $r->id_pengawas
