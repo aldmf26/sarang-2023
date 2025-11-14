9de9268a152601631385773be17ecd5c2fdb70b0
nadws <nandw567@gmail.com>
2025-11-14 13:13:43 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -1192,7 +1192,7 @@ ORDER BY  tgl ASC;");
   left join tb_anak as e on e.id_anak = c.id_anak
   left join hasil_wawancara as f on f.id_anak = e.id_anak
   left join tb_hancuran as g on g.no_box = c.no_box and g.kategori = 'cetak'
-  where c.no_box != '9999'
+  where c.no_box != '9999' and c.id_kelas not in('126','166','152','142')
     
     UNION ALL 
     
@@ -1218,7 +1218,7 @@ ORDER BY  tgl ASC;");
   left join tb_anak as e on e.id_anak = c.id_anak
   left join hasil_wawancara as f on f.id_anak = e.id_anak
   left join tb_hancuran as g on g.no_box = c.no_box and g.kategori = 'cetak'
-  where c.no_box != '9999'
+  where c.no_box != '9999' and c.id_kelas not in('126','166','152','142')
 ) AS hasil
 WHERE tgl = '$r->tgl' and id_pengawas = $r->id_pengawas
 group by tgl, no_box
