23e6a2d9a2efffd129310727173d87307eecde20
nadws <nandw567@gmail.com>
2025-11-17 11:43:45 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -647,7 +647,7 @@ SELECT d.tgl_ambil as tgl, d.tgl_serah as tgl_selesai, d.no_box, f.nm_partai, g.
 
         $data = DB::select("SELECT a.tgl, a.grade, a.nm_partai,
         sum(a.pcs) as pcs, sum(a.gr) as gr, count(a.box_pengiriman) as box FROM grading_partai as a 
-        where a.tgl = '$r->tgl'
+        where a.tgl = '$r->tgl' and a.nm_partai != 'BJM 1001'
         group by a.grade , a.nm_partai
         order by a.nm_partai ASC, a.grade ASC;");
         return response()->json([
