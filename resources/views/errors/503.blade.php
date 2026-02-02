42c72ef925d3bb54f87d8a0467b00e51628919f7
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2026-02-02 09:35:08 +0800

    update gambar

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -16,8 +16,10 @@
         <div class="error-page container" style="margin-top:-110px;">
             <div class="col-md-8 col-12 offset-md-2">
                 <div class="text-center">
-                    <img class="img-error" src="{{ asset('theme') }}/assets/images/samples/error-500.svg"
+                    <img class="img-error" src="{{ asset('theme') }}/assets/images/samples/sinta.jpeg"
                         alt="Not Found">
+                    {{-- <img class="img-error" src="{{ asset('theme') }}/assets/images/samples/error-500.svg"
+                        alt="Not Found"> --}}
                     <h1 class="error-title">PROGRAM MAINTANCE</h1>
                     <p class='fs-5 text-gray-600'>Karena sedang opname barang.</p>
 
