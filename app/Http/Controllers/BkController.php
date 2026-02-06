51e6c7ad23ad9d775b7955b3ad03275329225a9f
nadws <nandw567@gmail.com>
2026-02-06 10:14:23 +0800

    update BkController.php

--- app/Http/Controllers/BkController.php
+++ app/Http/Controllers/BkController.php
@@ -227,7 +227,7 @@ class BkController extends Controller
                         DB::table('bk')->insert([
                             'no_lot' => '0',
                             'nm_partai' => $row[0],
-                            'no_box' => $nobox,
+                            'no_box' => $row[9],
                             'tipe' => $row[1],
                             'ket' => $row[2],
                             'warna' => $row[3],
