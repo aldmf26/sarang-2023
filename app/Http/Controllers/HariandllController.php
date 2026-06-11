b61c8c183825a90df7ddf20cfad5a3bd45923d6e
nadws <nandw567@gmail.com>
2026-06-11 13:11:15 +0800

    update HariandllController.php, getdata.blade.php and resources/views/home/cetak_new/index.blade.php

--- app/Http/Controllers/HariandllController.php
+++ app/Http/Controllers/HariandllController.php
@@ -37,7 +37,7 @@ class HariandllController extends Controller
             'bulan' => DB::table('bulan')->get(),
             'datas' => DB::table('tb_hariandll  as a')
                 ->join('tb_anak as b', 'a.id_anak', 'b.id_anak')
-                ->where([['ditutup', 'T'], ['b.id_pengawas', $id_user], ['kategori', $kategori]])
+                ->where([['ditutup', 'T'], ['a.id_pengawas', $id_user], ['kategori', $kategori]])
                 // ->whereBetween('a.tgl', [$tgl1, $tgl2])
                 ->orderBy('a.id_hariandll', 'DESC')
                 ->get()
