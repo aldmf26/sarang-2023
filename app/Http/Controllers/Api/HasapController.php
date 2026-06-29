0fb7871b14d2b6d622288ba43983ced9870a2f59
nadws <nandw567@gmail.com>
2026-06-29 08:46:37 +0800

    update HasapController.php

--- app/Http/Controllers/Api/HasapController.php
+++ app/Http/Controllers/Api/HasapController.php
@@ -465,7 +465,7 @@ SELECT d.tgl_ambil as tgl, d.tgl_serah as tgl_selesai, d.no_box, f.nm_partai, g.
             FROM pengiriman_packing_list as c 
             group by c.no_nota
         ) as c on c.no_nota = b.no_nota
-    WHERE b.tgl_input = '$tgl' and c.tujuan ='hk'
+    WHERE b.tgl_input = '$tgl' 
     GROUP BY b.no_barcode, gp.grade
 
         ");
